f23e90a6c7716ed3f4d60920373438fc3785efc2
Guangyuan Yang <yzgyyang@outlook.com>
2019-03-11 19:51:33 -0700

    Remove db for now

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from app import views
 
 app = Flask(__name__)
 app.config.from_object('config')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db = SQLAlchemy(app)
-db.create_all()
-
-from app import views
